763343cd1d774b52a7272f1d52b118e154105869
Kwon Joon Young <112603864+kjmnemo@users.noreply.github.com>
2024-07-18 19:34:13 +0900

    Title: 최고의 33위치, Time: 154ms, Memory: 28MB, Status: Passed - CodeTree

--- 240718/최고의 33위치/best-place-of-33.py
+++ 240718/최고의 33위치/best-place-of-33.py
@@ -2,8 +2,8 @@ n = int(input())
 arr = [list(map(int,input().split())) for _ in range(n)]
 
 ans = 0
-for i in range(n-3):
-    for j in range(n-3):
+for i in range(n-2):
+    for j in range(n-2):
         tmp = 0
         for p in range(3):
             for q in range(3):
@@ -11,6 +11,7 @@ for i in range(n-3):
         ans = max(tmp, ans)
 
 if n == 3:
+    ans = 0
     for p in range(3):
         for q in range(3):
             ans += arr[p][q]
