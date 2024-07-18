244d1983a7a7dfedcb05bebe8b34a756ccd34a6f
Kwon Joon Young <112603864+kjmnemo@users.noreply.github.com>
2024-07-18 21:25:57 +0900

    Title: 연속된 숫자 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240718/연속된 숫자 만들기/create-consecutive-numbers.py
+++ 240718/연속된 숫자 만들기/create-consecutive-numbers.py
@@ -8,28 +8,47 @@
 lst = sorted(list(map(int, input().split())))
 ans = []
 
+a = lst[0]
+b = lst[1]
+c = lst[2]
+
+if abs(a-b)==1 and abs(b-c) == 1:
+    print(0)
+elif abs(a-b) ==2 or abs(b-c) ==2:
+    print(1)
+else:
+    print(2)
+
+ans = 0
+b = set()
 def dfs(alst,m):
-    #print(alst)
     alst = sorted(alst)
+    #print(alst,m)
     a1 = alst[0]
     a2 = alst[1]
     a3 = alst[2]
     global ans
 
     if abs(a1-a2)==1 and abs(a2-a3)==1:
-        ans.append(m)
+        ans = max(ans,m)
         return
 
     if abs(a2-a3) > 1:
-        for j in range(a2+1,a3):
-            nlst = [a2,a3] + [j]
-            dfs(nlst, m+1)
+        if (a2,a3) in b:
+            return
+        else:
+            for j in (a2+1,a3-1):
+                nlst = [a2,a3] + [j]
+                b.add((a2,a3))
+                dfs(nlst, m+1)
 
     if abs(a1-a2) > 1:
-        for j in range(a1+1,a2):
+        if (a1,a2) in b:
+            return
+        for j in (a1+1,a2-1):
             nlst = [a1,a2] + [j]
+            b.add((a1,a2))
             dfs(nlst, m+1)
 
 dfs(lst,0)
-print(min(ans))
-print(max(ans))
+print(ans)
