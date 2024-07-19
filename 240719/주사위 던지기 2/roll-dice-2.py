c3c2119979688e0e8c3b755455d4ee91629dac63
Kwon Joon Young <112603864+kjmnemo@users.noreply.github.com>
2024-07-19 11:29:02 +0900

    Title: 주사위 던지기 2, Time: 315ms, Memory: 29MB, Status: Passed - CodeTree

--- 240719/주사위 던지기 2/roll-dice-2.py
+++ 240719/주사위 던지기 2/roll-dice-2.py
@@ -1,11 +1,13 @@
 n, m = map(int, input().split())
 
 def dfs(cnt, result):
-    if cnt == n and sum(result)==m:
-        print(*result)
+    if cnt == n:
+        if sum(result)==m:
+            print(*result)
         return
+
     for i in range(1,7):
-        if sum(result) + i + cnt - n > m:
+        if sum(result) + i + cnt - n+1 > m:
             continue
         else:
             dfs(cnt+1, result+[i])
