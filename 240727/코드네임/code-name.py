dca91827f2908faebb7a589d32718a7af950172b
Kwon Joon Young <112603864+kjmnemo@users.noreply.github.com>
2024-07-27 14:33:20 +0900

    Title: 코드네임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240727/코드네임/code-name.py
@@ -0,0 +1,14 @@
+class Agent:
+    def __init__(self, codename, score):
+        self.codename = codename
+        self.score = int(score)
+
+agents = []
+for _ in range(5):
+    codename, score = input().split()
+    agents.append(Agent(codename, score))
+
+lowest= min(agents, key=lambda agent: agent.score)
+
+
+print(f'{lowest_score_agent.codename} {lowest.score}')
